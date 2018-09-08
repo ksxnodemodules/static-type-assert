1a6b9c117b9bc22375e8eac85e4baad65fa6e7d5
khai96_ <hvksmr1996@gmail.com>
2018-09-09 02:42:11 +0700

    Fix definition

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ import {SetComplement} from 'utility-types'
 
 declare namespace all {
   function assert<Type> (...values: Type[]): typeof assert
-  function complement<Include, Exclude> (...values: SetComplement<Include, Exclude>[]): typeof complement
+  function complement<Include, Exclude extends Include> (...values: SetComplement<Include, Exclude>[]): typeof complement
 }
 
 export = all
