52c2d2feb806eeaaf65363af604d16d79fdb180d
khai96_ <hvksmr1996@gmail.com>
2018-09-13 18:04:00 +0700

    Shorter lines

--- index.d.ts
+++ index.d.ts
@@ -6,9 +6,15 @@ declare namespace assert {
   function extension<A, B> (expect: Extends<A, B>): typeof assert
   function equality<A, B> (expect: Equal<A, B>): typeof assert
   function inequality<A, B> (expect: NotEqual<A, B>): typeof assert
-  type Extends<A, B, True = true, False = false> = A extends B ? True : False
-  type Equal<A, B, True = true, False = false> = Extends<A, B, Extends<B, A, True, False>, False>
-  type NotEqual<A, B, True = true, False = false> = Equal<A, B, False, True>
+
+  type Extends<A, B, True = true, False = false> =
+    A extends B ? True : False
+
+  type Equal<A, B, True = true, False = false> =
+    Extends<A, B, Extends<B, A, True, False>, False>
+
+  type NotEqual<A, B, True = true, False = false> =
+    Equal<A, B, False, True>
 }
 
 export = assert
